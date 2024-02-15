9d9a5b6d3586ae67c048371fbce0593a82c949e4
Igor Silva <igorsilvauk@gmail.com>
2024-02-15 13:45:39 +0000

    added scores and loop

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
 let startGame = confirm('Do you want to play a game of Rock, Paper and Scissor?')
+let score = 0;
+let pcScore = 0;
 
 while(startGame === true){
     confirm('Welcome to the Game! \nYou will input 1 for Rock, 2 for Paper or 3 for Scissor. \nAre you ready to begin?')
     
+    let playAgain;
     let guess = Number(prompt('3, 2, 1, Go!!! \nRemember that 1 is for Rock, 2 is for Paper and 3 is for Scissors.'));
     let answer = Math.ceil(Math.random() * 3)
 
     //make a if statement to compare guess to answer
     if(guess === 1 && answer === 3){
-        alert('Congratulations! Your rock beat the computers scissors.')
+        score++
+        alert(`Congratulations! Your rock beat the computers scissors.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     }else if (guess === 2 && answer === 1){
-        alert('Congratulations! Your paper beat the computers rock.')
+        score++
+        alert(`Congratulations! Your paper beat the computers rock.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     }else if (guess === 3 && answer === 2){
-        alert('Congratulations! Your scissors beat the computers paper.')
+        score++
+        alert(`Congratulations! Your scissors beat the computers paper.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     }
 
     if(guess === 1 && answer === 2){
-        alert('Unlucky! Your rock lost to the computers paper.')
+        pcScore++
+        alert(`Unlucky! Your rock lost to the computers paper.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     } else if(guess === 2 && answer === 3){
-        alert('Unlucky! Your paper lost to the computers scissors.')
+        pcScore++
+        alert(`Unlucky! Your paper lost to the computers scissors.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     } else if(guess === 3 && answer === 1){
-        alert('Unlucky! Your scissors lost to the computers rock.')
+        pcScore++
+        alert(`Unlucky! Your scissors lost to the computers rock.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     }
     
     if(guess === answer){
-        alert('Unlucky! You drew with the computer.')
+        score++
+        pcScore++
+        alert(`Unlucky! You drew with the computer.\nYour score now is: ${score}.\nThe computer score now is: ${pcScore}.`)
     }
 
     // need to add the following logic
@@ -32,7 +43,8 @@ while(startGame === true){
     // scissors 3 > paper 2
     // paper 2 > rock 1
 
-
-    
-    break;
+    playAgain = confirm('Do you want to play again?')
+    if(playAgain !== true){
+        break;
+    }
 }
